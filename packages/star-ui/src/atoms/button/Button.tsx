2918e6946f85fc2bcf97e9a047987568f7e9b16f
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-08-24 22:25:36 +0530

    Update button component with some new props

--- packages/star-ui/src/atoms/button/Button.tsx
+++ packages/star-ui/src/atoms/button/Button.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import Styled from 'styled-components';
 
-type ButtonType = 'submit' | 'button' | 'reset';
+type ButtonTypes = 'submit' | 'button' | 'reset';
+type ButtonVariants = 'primary' | 'secondary' | 'success' | 'danger' | 'ghost';
+type ButtonSizes = 'small' | 'normal' | 'big';
+
 interface IButtonProps {
-  type?: ButtonType;
+  type?: ButtonTypes;
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
   children: React.ReactNode;
 }
 const defaultProps = {
-  type: 'button'
+  type: 'button',
+  variant: 'secondary',
+  size: 'normal'
 };
+
+const ButtonElement = Styled.button``;
 
 const Button = (props: IButtonProps) => {
   const { type, children } = props;
 
-  return <button type={type}>{children}</button>;
+  return <ButtonElement type={type}>{children}</ButtonElement>;
 };
 
 Button.defaultProps = defaultProps;
