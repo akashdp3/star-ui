c5508dee7a8e20aea64a175096c64010abb58133
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-09-20 17:40:19 +0530

    Add a sample story to Text

---
+++ packages/star-ui/src/atoms/text/text.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Text from './Text';
+
+export default {
+  title: 'Text',
+  component: Text
+};
+
+/*
+ *  TODO:
+ *  [x] Sample Text
+ *  [ ] Text with font size props
+ *  [ ] Text with color props
+ *  [ ] Text with elipsis (with and without width specified)
+ *  [ ] Custom Styles
+ */
+
+export const Sample = () => {
+  return <Text>Sample Text</Text>;
+};
