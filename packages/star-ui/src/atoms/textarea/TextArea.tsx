8aae147fa3d1b0bd81110a5ce53e74e214b61487
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-10-29 11:42:11 +0530

    Create card component

--- packages/star-ui/src/atoms/textarea/TextArea.tsx
+++ packages/star-ui/src/atoms/textarea/TextArea.tsx
@@ -2,29 +2,30 @@ import React from 'react';
 import Styled from 'styled-components';
 
 interface ITextAreaProps {
-  placeholder?: string;
-  disabled?: boolean;
-  isInvalid?: boolean;
+    placeholder?: string;
+    disabled?: boolean;
+    isInvalid?: boolean;
 }
 const defaultProps = {
-  disabled: false,
-  isInvalid: false
+    disabled: false,
+    isInvalid: false
 };
 
 const TextAreaElement = Styled.textarea`
   width: 100%;
   padding: 8px;
   border: 1px solid ${(props: any) =>
-    props.isInvalid
-      ? props.theme.colors.danger.base
-      : props.theme.colors.secondary.base};
+      props.isInvalid
+          ? props.theme.colors.danger.base
+          : props.theme.colors.secondary.base};
   border-radius: 4px;
+  outline: none;
 
   &:focus {
     border-color: ${(props: any) =>
-      props.isInvalid
-        ? props.theme.colors.danger.base
-        : props.theme.colors.primary.dark};
+        props.isInvalid
+            ? props.theme.colors.danger.base
+            : props.theme.colors.primary.dark};
   }
 
   &:disabled {
@@ -35,7 +36,7 @@ const TextAreaElement = Styled.textarea`
 `;
 
 const TextArea = (props: ITextAreaProps) => {
-  return <TextAreaElement {...props} />;
+    return <TextAreaElement {...props} />;
 };
 
 export default TextArea;
