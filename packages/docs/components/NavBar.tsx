9f9b945a99c65444f752fb7cecd23e63cbbd39db
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-11-09 23:45:05 +0530

    Remove unnecessary icons

--- packages/docs/components/NavBar.tsx
+++ packages/docs/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaDev, FaGithub, FaTwitter, FaMoon, FaSun } from 'react-icons/fa';
+import { FaGithub, FaMoon, FaSun } from 'react-icons/fa';
 
 import Icon from './Icon';
 
@@ -16,15 +16,7 @@ type Social = {
 const SOCIAL_LINKS = {
     github: {
         name: 'Github',
-        link: 'https://github.com/akashdsamantra'
-    },
-    twitter: {
-        name: 'Twitter',
-        link: 'https://twitter.com/akashdsamantra'
-    },
-    dev: {
-        name: 'Dev.to',
-        link: 'https://dev.to/akashdsamantra'
+        link: 'https://github.com/akashdsamantra/star-ui'
     }
 };
 
@@ -39,18 +31,10 @@ const NavBar = (props: INavBarProps) => {
         <div className="border-solid">
             <nav className="flex items-center p-4 justify-between bg-white">
                 <h1>Heading</h1>
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid grid-cols-2 gap-4">
                     <Icon
                         icon={FaGithub}
                         onClick={openSocialLink(SOCIAL_LINKS.github)}
-                    />
-                    <Icon
-                        icon={FaTwitter}
-                        onClick={openSocialLink(SOCIAL_LINKS.twitter)}
-                    />
-                    <Icon
-                        icon={FaDev}
-                        onClick={openSocialLink(SOCIAL_LINKS.dev)}
                     />
                     <Icon
                         icon={theme === 'light' ? FaSun : FaMoon}
