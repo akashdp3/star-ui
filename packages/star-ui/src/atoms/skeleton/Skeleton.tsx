2abc385e2cce39d0511944379656e85f201f23a1
Akashdeep Samantra <akash.samantra19@gmail.com>
2021-02-04 23:40:32 +0530

    Update skeleton component

--- packages/star-ui/src/atoms/skeleton/Skeleton.tsx
+++ packages/star-ui/src/atoms/skeleton/Skeleton.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Styled, { keyframes } from 'styled-components';
 
-interface ISkeleton {
-    /* if true, show skeleton. Otherwise return children */
-    isLoading?: Boolean;
+interface ISkeletonItem {
+    /* id: Unique identifier required only in case of config */
+    id?: number;
     /* children: prefer string type */
     children?: React.ReactNode;
+    /* if true, show skeleton. Otherwise return children */
+    isLoading?: Boolean;
     /* Skeleton height. Default height: inherit */
     height?: String;
     /* Skeleton width. Default width: 100% */
@@ -17,7 +19,15 @@ interface ISkeleton {
     /* Repeat count for skeleton. Default: 1 */
     repeatCount?: Number;
 }
-const DEFAULT_PROPS = {
+interface ISkeleton {
+    /* children: prefer string type */
+    children?: React.ReactNode;
+    /* config: Configuration for skeeton */
+    config?: Array<ISkeletonItem>;
+    /* isLoading: if true, show skeleton. Otherwise return children */
+    isLoading?: Boolean;
+}
+const SKELETON_ITEM_DEFAULT_PROPS = {
     isLoading: true,
     height: 'inherit',
     width: '100%',
@@ -25,41 +35,47 @@ const DEFAULT_PROPS = {
     speed: '1200ms',
     repeatCount: 1
 };
+const SKELETON_DEFAULT_PROPS = {
+    isLoading: true,
+    config: []
+}
 
+/* Skeleton animation */
 const shine = keyframes`
-    0% { background-position: 100% 50%; }
-    100% { background-position: -100% 50%; }
+  0% { background-position: 100% 50%; }
+  100% { background-position: -100% 50%; }
 `;
+
 const StyledSkeleton = Styled.div`
-    margin-bottom: ${(props: any) => props.marginBottom || '16px'};
+    margin-bottom: ${(props: any) => props.marginBottom || "16px"};
     box-sizing: border-box;
-    height: ${(props: any) => props.height || 'inherit'};
-    animation-duration: ${(props: any) => props.speed || '1200ms'};
-    width: ${(props: any) => props.width || '100%'};
+    height: ${(props: any) => props.height || "inherit"};
+    animation-duration: ${(props: any) => props.speed || "1200ms"};
+    width: ${(props: any) => props.width || "100%"};
     animation-name: ${shine};
     animation-iteration-count: infinite;
     animation-timing-function: linear;
-    border-radius: ${(props: any) => props.borderRadius || '2px'};
-    background-image: linear-gradient(90deg, lightGrey 0%, lightGrey 20%, transparent 50%, lightGrey 80%, lightGrey 100%);
+    border-radius: ${(props: any) => props.borderRadius || "2px"};
+    background-image: linear-gradient(90deg, rgb(221, 221, 221, 0.6) 0%, rgb(221, 221, 221, 0.6) 20%, transparent 50%, rgb(221, 221, 221, 0.6) 80%, rgb(221, 221, 221, 0.6) 100%);
     background-position-x: 0%;
     background-position-y: 0%;
     background-size: 200% 200%;
 `;
 
-const range = (count: Number = 1) => {
+const range = (count: Number) => {
     return Array(count)
         .fill(1)
         .map((_, index) => index + 1);
 };
 
-const Skeleton = (props: ISkeleton) => {
+const SkeletonItem = (props: ISkeletonItem) => {
     const { isLoading, children, repeatCount, ...others } = props;
-    const lines = range(repeatCount);
+    const lines = range(repeatCount || 1);
 
     if (isLoading) {
         return (
             <>
-                {lines.map((line) => (
+                {lines.map(line => (
                     <StyledSkeleton {...others} key={line} />
                 ))}
             </>
@@ -69,5 +85,29 @@ const Skeleton = (props: ISkeleton) => {
     return <div>{children}</div>;
 };
 
-Skeleton.defaultProps = DEFAULT_PROPS;
+SkeletonItem.defaultProps = SKELETON_ITEM_DEFAULT_PROPS;
+
+const Skeleton = (props: ISkeleton) => {
+    const { isLoading, config, children, ...others } = props;
+
+    if (isLoading && config && config.length > 0) {
+        return (
+            <>
+                {config.map(x => (
+                    <React.Fragment key={x.id || null}>
+                        <SkeletonItem {...x} isLoading={isLoading} />
+                    </React.Fragment>
+                ))}
+            </>
+        );
+    }
+
+    return (
+        <SkeletonItem {...others} isLoading={isLoading || false}>
+            {children}
+        </SkeletonItem>
+    );
+};
+
+Skeleton.defaultProps = SKELETON_DEFAULT_PROPS;
 export default Skeleton;
