69d15b5131fad8acb406852823779ff82c7c7268
Akashdeep Samantra <akash.samantra19@gmail.com>
2021-03-10 08:37:34 +0530

    Handle style for all states in checkbox

--- packages/star-ui/src/atoms/checkbox/Checkbox.tsx
+++ packages/star-ui/src/atoms/checkbox/Checkbox.tsx
@@ -4,7 +4,7 @@ import Styled from 'styled-components';
 
 interface ICheckBoxProps {
     label?: string;
-    onChange?: Function;
+    onChange?: Function; // TODO: Change this to HTML event
     disabled?: boolean;
     checked?: boolean;
 }
@@ -19,7 +19,7 @@ const StyledCheckBoxContainer = Styled.label`
     width: fit-content;
     display: block;
     position: relative;
-    padding-left: 25px;
+    padding-left: 24px;
     cursor: ${(props: any) => (props.disabled ? 'not-allowed' : 'pointer')};
     -webkit-user-select: none;
     -moz-user-select: none;
@@ -27,6 +27,21 @@ const StyledCheckBoxContainer = Styled.label`
     user-select: none;
     font-size:14px;
 
+    .label {
+        ${(props: any) =>
+            props.disabled &&
+            `
+                color: ${props.theme.colors.secondary.light};
+            `}
+        padding-top: 2px;
+    }
+
+    input {
+        position: absolute;
+        opacity: 0;
+        height: 0;
+        width: 0;
+    }
 
     /* On mouse-over, add a grey background color */
     &:hover input:not(:disabled) ~ .checkmark {
@@ -36,13 +51,6 @@ const StyledCheckBoxContainer = Styled.label`
     /* When the checkbox is checked, add a blue background */
     input:checked ~ .checkmark {
         background-color: ${(props: any) => props.theme.colors.primary.base};
-    }
-
-    input:disabled ~ .checkmark {
-        /* border-color: ${(props: any) =>
-            props.theme.colors.secondary.light}; */
-        /* background-color: ${(props: any) =>
-            props.theme.colors.secondary.light}; */
     }
 
     /* Show the checkmark when checked */
@@ -56,19 +64,13 @@ const StyledCheckBoxContainer = Styled.label`
         top: 2px;
         width: 4px;
         height: 8px;
-        border: solid white;
+        border: 1px solid ${(props: any) =>
+            props.theme.colors.secondary.lighter};
         border-width: 0 1px 1px 0;
         -webkit-transform: rotate(45deg);
         -ms-transform: rotate(45deg);
         transform: rotate(45deg);
     }
-`;
-
-const StyledCheckBox = Styled.input`
-    position: absolute;
-    opacity: 0;
-    height: 0;
-    width: 0;
 `;
 
 const StyledCheckBoxSpan = Styled.span`
@@ -78,7 +80,7 @@ const StyledCheckBoxSpan = Styled.span`
     left: 0;
     height: 16px;
     width: 16px;
-    border: 1px solid ${(props: any) => props.theme.colors.secondary_lighter};
+    border: 1px solid ${(props: any) => props.theme.colors.secondary.lighter};
     border-radius: 4px;
     cursor: ${(props: any) => (props.disabled ? 'not-allowed' : 'pointer')};
 
@@ -89,19 +91,14 @@ const StyledCheckBoxSpan = Styled.span`
         display: none;
     }
 `;
-const StyledCheckBoxValue = Styled.span`
-
-`;
 
 const CheckBox = (props: ICheckBoxProps) => {
     const { label, disabled, ...others } = props;
 
     return (
-        <StyledCheckBoxContainer className="container" disabled={disabled}>
-            <StyledCheckBoxValue disabled={disabled}>
-                {label}
-            </StyledCheckBoxValue>
-            <StyledCheckBox type="checkbox" disabled={disabled} {...others} />
+        <StyledCheckBoxContainer disabled={disabled}>
+            <span className="label">{label}</span>
+            <input type="checkbox" disabled={disabled} {...others} />
             <StyledCheckBoxSpan className="checkmark" disabled={disabled} />
         </StyledCheckBoxContainer>
     );
