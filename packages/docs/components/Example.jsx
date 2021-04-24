c933ea9e4f97059e6d44289f142d1eb0e1f5bea8
Akashdeep Samantra <akash.samantra19@gmail.com>
2021-04-24 17:36:46 +0530

    Delete docs project

--- packages/docs/components/Example.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme, Alert } from 'react-star-ui';
-
-const Example = ({ children }) => {
-    return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
-};
-
-export default Example;
